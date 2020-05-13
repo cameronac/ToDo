02cf8df8506028996d938f858d8ff1c9bd032903
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-12 21:56:21 -0500

    Added TaskRepresentation Model and TaskTableViewCell

---
+++ ToDo/Views/TaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskTableViewCell.swift
+//  ToDo
+//
+//  Created by Cameron Collins on 5/12/20.
+//  Copyright © 2020 iOS BW. All rights reserved.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
