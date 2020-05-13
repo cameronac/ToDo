148587899f3b8a8392fc7b87793e417c126a77fb
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-12 21:42:37 -0500

    Added CoreDataModel

---
+++ ToDo/CoreData/CoreDataStack.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataStack.swift
+//  ToDo
+//
+//  Created by Cameron Collins on 5/12/20.
+//  Copyright © 2020 iOS BW. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class CoreDataStack {
+
+    //Singleton
+    static var shared = CoreDataStack() //CoreDataStack Object
+    
+    //Create CoreDataModel for Tasks
+    var container: NSPersistentContainer {
+        let container = NSPersistentContainer(name: "TasksToDo")    //Get the CoreDataModel by Name
+        
+        //Completes the Creation of the CoreDataStack
+        container.loadPersistentStores { (description, error) in
+            
+            //Error Checking
+            if let error = error {
+                print("Error Loading Persistent Stores in: \(#file): \(#function): \(#line): Error: \(error)")
+            }
+        }
+        
+        return container
+    }
+    
+    
+    //Getting mainContext
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    
+    
+    
+}
