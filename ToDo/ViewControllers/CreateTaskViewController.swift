e8f53c33f299c9310b1af1cfb5485501d2d98b4a
Cameron Collins <cameroncollins19@yahoo.com>
2020-05-12 23:29:57 -0500

    Added CreateTaskViewController

---
+++ ToDo/ViewControllers/CreateTaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CreateTaskViewController.swift
+//  ToDo
+//
+//  Created by Cameron Collins on 5/12/20.
+//  Copyright © 2020 iOS BW. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
